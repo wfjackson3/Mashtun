3bbeb35539a41515b6a3150a4943f8f5b642cb65
wfjackson3 <willis@mashtunbeer.com>
2011-01-17 22:03:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mashtun::Application.config.secret_token = 'd311ad42789c5878350c346c50ece18b8715559ec71af75e32961fa97ca909a9ef2051de3f68f46a017620da2d499422e63454576692fe724015b998f75bae91'
