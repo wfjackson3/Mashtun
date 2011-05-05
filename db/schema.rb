7e53db637a784686a8d169049ee502cef36c31c2
Craig S. Cottingham <craig.cottingham@gmail.com>
2011-05-04 21:07:36 -0500

    Migrate database
    Signed-off-by: Craig S. Cottingham <craig.cottingham@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110505020637) do
+
+  create_table "producers", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
